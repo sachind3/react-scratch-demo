d85ace52717eef27ed651862eb4cec92616598a7
sachind3 <desai.sachin45@gmail.com>
2021-02-22 13:22:36 +0530

    wip

--- src/User.js
+++ src/User.js
@@ -1,9 +1,9 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 
 function User() {
-  let { name } = useParams();
-  console.log(name);
-  return <div>hello user {name}</div>;
+  // let { name } = useParams();
+  // console.log(name);
+  return <div>hello user </div>;
 }
 
 export default User;
